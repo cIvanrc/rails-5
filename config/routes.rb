7982265c84d7d0309f8e22f399e71d00357d94d3
ivan <c.ivan.felix.r@gmail.com>
2019-04-28 19:55:09 -0500

    added login form / test on sessions / flash.now

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
-  get '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   resources :users
 end
