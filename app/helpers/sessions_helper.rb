0866b059b6a4c88744d697bf64297753f67294cc
ivan <c.ivan.felix.r@gmail.com>
2019-04-30 14:26:36 -0500

    Friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,7 +12,12 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  #returns the current logged-in user (if any).
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
+  #returns the  user corresponding to the remmeber token cookie.
   def current_user
     if ( user_id = session[:user_id] )
       @current_user ||= User.find_by(id: session[:user_id])
@@ -40,5 +45,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trygin to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
